375cc7e8813cd0500c359cfe3713900d6337085c
imm <malihanawshin@gmail.com>
2017-10-01 22:10:30 +0600

    home page problem solved

--- app/src/main/java/com/example/imm/citi/activities/SplashActivity.java
+++ app/src/main/java/com/example/imm/citi/activities/SplashActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.imm.citi.R;
+import com.example.imm.citi.technicalClasses.User;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -13,15 +14,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        int duration = 3000;
 
-        boolean postDelayed = new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
+        if (User.loggedIn) duration = 800;/*{
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+            finish();
+        } else*/ {
+            boolean postDelayed = new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
 
-                Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },3000);
+                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, duration);
+        }
     }
 }
